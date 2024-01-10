7f60af033633ee15e5211000bc6115ca471a7820
Diogo Correia <diogo_correia7@hotmail.com>
2024-01-10 00:26:24 +0000

    initial commit

---
+++ web_scraping.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+marca = input("Qual a marca do carro? ")
+modelo = input("Qual o modelo do carro? ")
+valor = input("Qual o valor máximo? ")
+
+url = f"https://www.standvirtual.com/carros/{marca}/{modelo}?search%5Bfilter_float_price%3Ato%5D={valor}&search%5Border%5D=created_at_first%3Adesc"
+page = requests.get(url).text
+doc = BeautifulSoup(page, "html.parser")
+
+links = doc.find_all(class_="e1oqyyyi9 ooa-1ed90th er34gjf0")
+prices = doc.find_all(class_="e1oqyyyi16 ooa-1n2paoq er34gjf0")
+
+for link, price in zip(links, prices):
+    link_name = link.get_text(strip=True)
+    link_href = link.find('a')['href']
+    price_text = price.get_text(strip=True)
+    print(f"Marca e Modelo: {link_name}\tLink: {link_href}\tPreço: {price_text}\n")
